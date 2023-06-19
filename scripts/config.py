4dde31fa35a85895dea010fb045871e05e68a53a
kiranbaby14 <kiranbaby256@gmail.com>
2023-06-19 13:39:33 +0100

    Modified config file to properly hide access token

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,15 @@
-ACCESS_TOKEN = 'Enter your token here'
+import os
 
 
 def get_access_token():
-    return ACCESS_TOKEN
+    token_file_path = os.path.expanduser('~') + '/.github_shell_token'
+    if os.path.isfile(token_file_path):
+        with open(token_file_path, 'r') as token_file:
+            access_token = token_file.read().strip()
+        if access_token:
+            # return token
+            return access_token
+        else:
+            print("Error: Empty access token in the file.")
+    else:
+        print("Error: Token file not found.")
