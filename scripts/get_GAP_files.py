727162b6e872b3b649463b07c50716343c47cf54
kiranbaby14 <kiranbaby256@gmail.com>
2023-07-08 11:35:44 +0100

    Save the downloadable file links to a txt file and add a processing animation

--- scripts/get_GAP_files.py
+++ scripts/get_GAP_files.py
@@ -1,5 +1,7 @@
 from github import Github
+
 import time
+import os
 import sys
 
 # append the path of the
@@ -18,9 +20,13 @@ def get_GAP_files(access_token, repo_list):
     :param repo_list: list containing repositories to be downloaded
     :return:
     """
-    
+
     # Create a PyGithub instance using the access token
     g = Github(access_token)
+
+    # Specify the output folder path
+    output_folder = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
+    os.makedirs(output_folder, exist_ok=True)  # Create the output folder if it doesn't exist
 
     # Iterate over the repositories
     for repo_path in repo_list:
@@ -31,13 +37,15 @@ def get_GAP_files(access_token, repo_list):
             # Retrieve all matching files in the repository
             matching_files = check_matching_files(repo, "")
 
-            print("Matching Files in", repo_path)
-            for file_name in matching_files:
-                print(file_name)
-            print()
+            output_file = os.path.join(output_folder, "real_GAP_files.txt")
+
+            with open(output_file, 'a') as file:
+                for file_name in matching_files:
+                    file.write(file_name + "\n")
+
         except Exception as e:
             print("\nRate limit exceeded (Wait for a few minutes...!)\n")
-            time.sleep(300)
+            time.sleep(10)
             continue
 
 
