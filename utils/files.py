cd3ea7bcc32100009528b231388ae36a0921b655
Renia Correya <122054528+reniacorreya@users.noreply.github.com>
2023-07-09 22:09:01 +0100

    feat: Update files.py to save GAP files details to csv file

--- utils/files.py
+++ utils/files.py
@@ -1,3 +1,4 @@
+import csv
 
 def check_matching_files(repo, directory):
     """
@@ -15,6 +16,21 @@ def check_matching_files(repo, directory):
         else:
             file_name = content.name
             if file_name.endswith((".g", ".gi", ".gd")):
-                print(content)
-                matching_files.append(file_name)
+                file_url = content.download_url
+                matching_files.append({'file_name': file_name, 'file_url': file_url})
     return matching_files
+
+
+def save_to_csv_file(csv_file_path, fieldnames, data):
+
+    # Write the data to a CSV file
+    with open(csv_file_path, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        
+        # Write the header row
+        writer.writeheader()
+
+        # Write the repository details
+        writer.writerows(data)
+
+    print(f"GAP files details saved to {csv_file_path}.")
