787b35851d0e2cc4c5dd8c97cef88edf3f60d54c
Renia Correya <122054528+reniacorreya@users.noreply.github.com>
2023-07-09 22:05:29 +0100

    feat: Update get_GAP_files.py to save data to a csv file

--- scripts/get_GAP_files.py
+++ scripts/get_GAP_files.py
@@ -7,6 +7,7 @@ import sys
 sys.path.append("..")
 
 from utils.files import check_matching_files
+from utils.files import save_to_csv_file
 from utils.config import get_access_token
 
 
@@ -22,23 +23,52 @@ def get_GAP_files(access_token, repo_list):
     # Create a PyGithub instance using the access token
     g = Github(access_token)
 
+    # Initialise csv file name
+    csv_file_path = 'gap_files_details.csv'
+    gap_files_details = []
+
+    # Initialise column names in the csv file
+    fieldnames = ['Repository Name', 'Repository URL', 'Created At', 'Updated At', 'File Name', 'File URL']
+
     # Iterate over the repositories
     for repo_path in repo_list:
         # Get the repository object using PyGithub
         try:
             repo = g.get_repo(repo_path)
+            
+            # Extract the repository details
+            repo_name = repo.name
+            repo_url = repo.html_url
+            repo_created_at = repo.created_at
+            repo_updated_at = repo.updated_at
+
+            # Extract collaborator details
+            #collaborators = repo.get_collaborators()
+            #collaborator_usernames = [collaborator.login for collaborator in collaborators]
+            #num_collaborators = len(collaborator_usernames)
 
             # Retrieve all matching files in the repository
             matching_files = check_matching_files(repo, "")
 
             print("Matching Files in", repo_path)
-            for file_name in matching_files:
-                print(file_name)
+            for file in matching_files:
+                print(file['file_name'])
+                
+                # Create a list of dictionaries with the repository and file details
+                gap_files_details.append({
+                        'Repository Name': repo_name,
+                        'Repository URL': repo_url,
+                        'Created At': repo_created_at,
+                        'Updated At': repo_updated_at,
+                        'File Name': file['file_name'],
+                        'File URL': file['file_url']
+                    })
             print()
         except Exception as e:
-            print("\nRate limit exceeded (Wait for a few minutes...!)\n")
+            print("Exception occurred. Wait for a few minutes...\nException details:",e)
             time.sleep(300)
             continue
+    save_to_csv_file(csv_file_path, fieldnames, gap_files_details)
 
 
 def main():
